96e9efedfb30a293a73758194d89afcabbad8be5
Optini LLC <optini@Optinis-MacBook-Pro.local>
2013-03-13 18:14:41 +0500

    Initial Commit

---
+++ TestApplication/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestApplication
+//
+//  Created by Optini LLC on 3/13/13.
+//  Copyright (c) 2013 Optini LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TestApplication/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestApplication
+//
+//  Created by Optini LLC on 3/13/13.
+//  Copyright (c) 2013 Optini LLC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
